2c118df0fcbdcd78255cacf7b09609193cebf3c9
Clinton Aquilina <clinton.aquilina@outlook.com>
2022-09-22 20:41:47 +0200

    [OTHER] Source Code Comment Improvement In The Clock Class *Comments in the Clock class were added and improved, to make the source code more understandable.

--- ClockPatienceApp/GameItems/Clock.cs
+++ ClockPatienceApp/GameItems/Clock.cs
@@ -7,19 +7,24 @@ using System.Threading.Tasks;
 
 namespace ClockPatienceApp.GameItems
 {
+    /// <summary>
+    /// Class <c>Clock</c> represents the 13 hour hand clock. It consists of a double nested string list, with each list representing an hour hand.
+    /// </summary>
     internal class Clock
     {
+        // Double nested string list representing an hour hand which will hold another list string representing a pile of game cards.
         private List<List<string>>? _hourHands;
 
+        // Accessors for the HourHands string list
         public List<List<string>>? HourHands
         {
             get { return _hourHands; }
             set { _hourHands = value; }
         }
 
+        // Constructor for the clock class
         public Clock()
         {
-
             HourHands = new List<List<string>>();
         }
 
