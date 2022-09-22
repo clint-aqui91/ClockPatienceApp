6ae3ec7ea8a0245860d01fd7d13ce82b4fd72a00
Clinton Aquilina <clinton.aquilina@outlook.com>
2022-09-22 21:00:43 +0200

    [OTHER] Source Code Comment Improvement In The DeckOfCards Class *Comments in the DeckOfCards class were added and improved, to make the source code more understandable.

--- ClockPatienceApp/GameItems/DeckOfCards.cs
+++ ClockPatienceApp/GameItems/DeckOfCards.cs
@@ -8,11 +8,14 @@ using System.Threading.Tasks;
 
 namespace ClockPatienceApp.GameItems
 {
+    /// <summary>
+    /// Class <c>DeckOfCards</c> represents a deck of cards made up of 52 cards. It consists primarly consists of a string list.
+    /// </summary>
     internal class DeckOfCards
     {
         private List<string>? _deckOfCardsList;
 
-        // Accessor for the Deck of Cards list
+        // Get & Set Accessors for the Deck of Cards list
         public List<string>? DeckOfCardsList
         {
             get { return _deckOfCardsList; }
@@ -26,38 +29,50 @@ namespace ClockPatienceApp.GameItems
             DeckOfCardsList = BuildDeckOfCards(DeckOfCardsList);
         }
 
+        /// <summary>
+        /// Method <c>BuildDeckOfCards</c> generates a string list containing the cards.
+        /// </summary>
         private List<string> BuildDeckOfCards(List<string> deckOfCards)
         {
-            // Here we are ensuring that the List is cleared (any elements and indexes are emptied, in case this method is called from multiple occurences of the application and is passed as an argument containing any elements or indexes.
+            // Here we are ensuring that the List is cleared (any elements and indexes are emptied, in case this method is called from multiple occurences of the application or
+            // is passed as an argument containing any elements or indexes.
             deckOfCards.Clear();
+
             string Ranks = "Ranks";
             string Suits = "Suits";
             string NewDeckOfCards = "New Deck Of Cards";
             string ShuffledDeckOfCards = "Shuffled Deck Of Cards";
 
-
             List<string> cardRanks = new List<string>();
             List<string> cardSuits = new List<string>();
 
+            // Card ranks and suits are generated using the GenerateCardComponents method, based on the passed arguments into it.
             cardRanks = GenerateCardComponents(Ranks, cardRanks);
 
             cardSuits = GenerateCardComponents(Suits, cardSuits);
 
+            // Deck of cards (string list) is generated using the previously generated card ranks and suits and displayed inside the console / User Interface.
             deckOfCards = GenerateDeckOfCards(cardRanks, cardSuits);
             PrintDeckOfCards(NewDeckOfCards, deckOfCards);
 
+            // The deck of cards are shuffled and printed/outputted inside the console / User Interface.
             deckOfCards = ShuffleDeckOfCards(deckOfCards);
             PrintDeckOfCards(ShuffledDeckOfCards, deckOfCards);
 
             return deckOfCards;
         }
 
+        /// <summary>
+        /// Method <c>GenerateDeckOfCards</c> generates a deck of cards.
+        /// </summary>
         private static List<string> GenerateDeckOfCards(List<string> cardRanksList, List<string> cardSuitsList)
         {
             string Card;
 
             List<string> deckOfCards = new List<string>();
 
+            // 2 nested foreach loops which loops through each suit and rank found in their respective string list and adds them into a string, to create a card which in turn is added to the
+            // deck of cards string list.
             foreach (string Rank in cardRanksList)
             {
                 foreach (string Suit in cardSuitsList)
@@ -72,22 +87,28 @@ namespace ClockPatienceApp.GameItems
             return deckOfCards;
         }
 
+        /// <summary>
+        /// Method <c>ShuffleDeckOfCards</c> randomly shuffles (pseudo-random number generator) the deck of cards.
+        /// </summary>
         private List<string> ShuffleDeckOfCards(List<string> deckOfCards)
         {
             Random random = new Random();
-            //int i = 0;
             deckOfCards = deckOfCards.OrderBy(i => random.Next()).ToList();
-            //List<string> shuffled_list = deckOfCards.OrderBy(i => random.Next()).ToList(i => i.Key, i => i.Value);
-            //return (List<string>)deckOfCards.OrderBy(item => random.Next()).ToList();
-            //deckOfCards = deckOfCards.OrderBy(x => Guid.NewGuid()).ToList();
+
             return deckOfCards;
         }
 
+        /// <summary>
+        /// Method <c>GenerateCardComponents</c> generates the components of card (suits and ranks), depending on the passed component type string value.
+        /// </summary>
         private static List<string> GenerateCardComponents(string componentType, List<string> cardComponentsList)
         {
             string[] arrayOfRanks = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K" };
             string[] arrayOfSuits = { "H", "D", "C", "S" };
+            
             cardComponentsList.Clear();
+
+            // If component type value is ranks, generate a string list from the array containig the rank values.
             if (componentType == "Ranks")
             {
                 Console.WriteLine("\nRank List size before: ");
@@ -103,6 +124,7 @@ namespace ClockPatienceApp.GameItems
 
             }
 
+            // If component type value is suits, generate a string list from the array containig the suit values.
             if (componentType == "Suits")
             {
                 Console.WriteLine("\nSuits List size before: ");
@@ -117,9 +139,13 @@ namespace ClockPatienceApp.GameItems
                 Console.WriteLine("Suits List Count: " + Convert.ToString(cardComponentsList.Count));
 
             }
+
             return cardComponentsList;
         }
 
+        /// <summary>
+        /// Method <c>PrintCardComponents</c> outputs the card components (ranks or suits, depending on the passed arguments) to the console/user interface.
+        /// </summary>
         private static void PrintCardComponents(string componentName, List<string> cardComponents)
         {
             Console.WriteLine("\nCard " + componentName);
@@ -129,13 +155,16 @@ namespace ClockPatienceApp.GameItems
             }
         }
 
+        /// <summary>
+        /// Method <c>PrintDeckOfCards</c> outputs the deck of cards to the console/user interface.
+        /// </summary>
         internal protected static void PrintDeckOfCards(string deckOfCardsType, List<string> deckOfCards)
         {
             int CountOfCardsInDeck = 0;
             Console.WriteLine("\n" + deckOfCardsType);
+
             foreach (string CardInDeck in deckOfCards)
             {
-
 
                 //Console.WriteLine(CardInDeck);
                 CountOfCardsInDeck++;
